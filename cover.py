9416d64dc0a85e11398682544feccd156fd3db4f
owwe <berkan-yapici@hotmail.com>
2023-12-15 20:08:33 +0100

    berkan first commit

--- cover.py
+++ cover.py
@@ -1,13 +1,12 @@
 from docx import Document
 from docx.shared import Pt
 from sys import argv,exit
+import os
 
-
-if len(argv) < 4:
+if len(argv) < 3:
     print("usage: python cover.py position(ml,ds,cs) company(company name)")
     exit(0)
 print(len(argv))
-assert len(argv) < 4 == "usage: python cover.py position(ml,ds,cs) job_type(intern,full,part) company(company name)"
 
 position = argv[1]
 if len(argv) == 5:
@@ -20,35 +19,39 @@ document = Document()
 style = document.styles['Normal']
 font = style.font
 font.name = 'Calibri'#'Times New Roman'#'Calibri'
-font.size = Pt(13)
+font.size = Pt(11)
 
 greetings =document.add_paragraph("Dear Hiring Manager,")
 
 interest = {'cs':'programming','ml':'applications of machine learning','ds':'machine learning'}
 
-body = "I have graduated from Istanbul Technical University with a degree in electronics and communication engineering. "\
-       "I am currently studying Msc. machine learning at KTH Royal Institute of Technology in Stockholm. "\
-      f"My academic background and keen interest in {interest[position]} make me a perfect candidate for this position. "\
-       "I am good at adapting to new environments and working with a team."
+body = f'''
+I am writing to express my interest in the Data Scientist position at {company}, as advertised. With a strong background in machine learning and hands-on experience in implementing data processing workflows, developing neural networks, and applying advanced statistical analysis, I believe I am well-equipped to contribute effectively to your team.
 
+In my role as a Junior Data Scientist at PRICER, I successfully implemented and optimized data processing workflows using Google Cloud Functions, developed and maintained data pipelines with Google BigQuery, and applied advanced statistical analysis and machine learning models to extract meaningful insights from large datasets. The opportunity to visualize findings with Qlik Sense dashboards enhanced my communication skills, making complex data accessible to R&D team.
+
+My academic background includes an MSc in Machine Learning from KTH Royal Institute of Technology, where I acquired a solid foundation in machine learning techniques. Additionally, I have a degree in Electronics and Communication Engineering from Istanbul Technical University.
+
+I am proud to mention that I won the TUBITAK BIGG 2021 award, an Individual Young Entrepreneur program, supporting the early incubation of OMVISION. This startup focuses on the application of deep learning in the healthcare industry, showcasing my entrepreneurial spirit and commitment to innovation.
+'''
 
 body = document.add_paragraph(body)
+body.alignment = 0  # '3' corresponds to 'justify' alignment in docx
 
 bullet_point_entry = document.add_paragraph('I have experience in:')
 
 bullet_points = {'ml':[
-                    'Python’s data science libraries like pandas, scikit-learn, keras, tensorflow.',
-                    'Machine learning algorithms and applications',
-                    'Java'],
+                    'GCP, Google Big Query, Cloud Functions, Vertex AI, Qlik Sense',
+                    'pandas, scikit-learn, keras, tensorflow, flask, seaborn, matplotlib',
+                    'Github Actions, Terraform'],
                 'ds':[
-                    'Python’s data science libraries like pandas, scikit-learn, keras, tensorflow.',
-                    'Machine learning algorithms and applications',
-                    'Java',
-                    'SQL'],
+                    'GCP, Google Big Query, Cloud Functions, Vertex AI, Qlik Sense',
+                    'pandas, scikit-learn, keras, tensorflow, flask, seaborn, matplotlib',
+                    'Github Actions, Terraform'],
                 'cs':[
-                    'Python’s data science libraries like pandas, scikit-learn, keras, tensorflow.',
-                    'Machine learning algorithms and applications',
-                    'Java']
+                    'GCP, Google Big Query, Cloud Functions, Vertex AI, Qlik Sense',
+                    'pandas, scikit-learn, keras, tensorflow, flask, seaborn, matplotlib',
+                    'Github Actions, Terraform'],
                 }
 
 for point in bullet_points[position]:
@@ -56,14 +59,19 @@ for point in bullet_points[position]:
     bullet.style = 'List Bullet'
 
 
-summary =f'I believe all of these experiences will enable me to carry out a very successful job so that '\
-         f'I can build an expertise in this field. I sincerely claim my aspiration to be part of the {company} without '\
-         'frustrating the expectations on me throughout this journey.'
+summary =f'''
+I am excited about the opportunity to bring my technical skills, innovative mindset, and passion for data science to {company}. I am confident that my blend of academic knowledge, hands-on experience, and commitment to excellence makes me a strong candidate for this position.
+Thank you for considering my application. I look forward to the opportunity to discuss how my skills and experiences align with the goals of your team.
+'''
 summary = document.add_paragraph(summary)
 
 name = document.add_paragraph('Berkan Yapıcı')
 
 doc_name = f'cover_letter_{company}.docx'
+
+if not os.path.exists('cover_letters'):
+    os.mkdir('cover_letters')
+
 save_path = f'cover_letters/{doc_name}'
 document.save(save_path)
 
