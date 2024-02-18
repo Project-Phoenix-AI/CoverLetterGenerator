7d4c657deccb02d78c68a00acd950f3fbf0be9c5
owwe <berkan-yapici@hotmail.com>
2024-02-18 23:14:55 +0100

    wrong position input prevented

--- cover.py
+++ cover.py
@@ -9,11 +9,19 @@ if len(argv) < 3:
     print("usage: python cover.py position(ml,ds,cs) company(company name)")
     exit(0)
 
-position = argv[1]
+
 if len(argv) >= 3:
     company = get_company_name(argv)
 
+interest = {'cs':'software engineer','ml':'machine learning engineer','ds':'data scientist', 'deng':'data engineer', 'mlops':'MLOps engineer'}
+positions = {'cs':'software engineering','ml':'machine learning engineering','ds':'data science','deng':'data engineering','mlops':'MLOps engineering'}
 
+if argv[1] not in positions:
+    print('invalid position:', argv[1], )
+    print('valid positions:', list(positions.keys()))
+    exit(1)
+
+position = argv[1]
 document = Document()
 
 style = document.styles['Normal']
@@ -23,8 +31,7 @@ font.size = Pt(11)
 
 greetings =document.add_paragraph("Dear Hiring Manager,")
 
-interest = {'cs':'software engineer','ml':'machine learning engineer','ds':'data scientist', 'deng':'data engineer', 'mlops':'MLOps engineer'}
-positions = {'cs':'software engineering','ml':'machine learning engineering','ds':'data science','deng':'data engineering','mlops':'MLOps engineering'}
+
 body = f'''
 I am writing to express my interest in the {interest[position]} position at {company}, as advertised. With a strong background in machine learning and hands-on experience in implementing data processing workflows, developing neural networks, and applying advanced statistical analysis, I believe I am well-equipped to contribute effectively to your team.
 
@@ -43,19 +50,19 @@ bullet_point_entry = document.add_paragraph('I have experience in:')
 bullet_points = {'ml':[
                     'GCP, Google Big Query, Cloud Functions, Vertex AI, Qlik Sense',
                     'pandas, scikit-learn, keras, tensorflow, flask, seaborn, matplotlib',
-                    'Github Actions, Terraform'],
+                    'Github Actions, Terraform, Docker'],
                 'ds':[
                     'GCP, Google Big Query, Cloud Functions, Vertex AI, Qlik Sense',
                     'pandas, scikit-learn, keras, tensorflow, flask, seaborn, matplotlib',
-                    'Github Actions, Terraform'],
+                    'Github Actions, Terraform, Docker'],
                 'cs':[
                     'GCP, Google Big Query, Cloud Functions, Vertex AI, Qlik Sense',
                     'pandas, scikit-learn, keras, tensorflow, flask, seaborn, matplotlib',
-                    'Github Actions, Terraform'],
+                    'Github Actions, Terraform, Docker'],
                 'deng':[
                     'GCP, Google Big Query, Cloud Functions, Vertex AI, Qlik Sense',
                     'pandas, scikit-learn, keras, tensorflow, flask, seaborn, matplotlib',
-                    'Github Actions, Terraform']
+                    'Github Actions, Terraform, Docker'],
                 }
 
 for point in bullet_points[position]:
@@ -84,9 +91,7 @@ save_path = f'cover_letters/docs/{doc_name}'
 document.save(save_path)
 print('cover letter:', company, '\nposition:' ,positions[position])
 
-# file = open(save_path, "w")
-# convert(f'/cover_letters/docs/{doc_name}', f'/cover_letters/pdf/{doc_name}.pdf')
-# file.close()
+
 
 
 
